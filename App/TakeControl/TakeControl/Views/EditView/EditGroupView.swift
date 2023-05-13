5afadf9b6177e97514b4737109b271db32409e44
BwezB <blaz.bergant4@gmail.com>
2023-05-13 23:37:55 +0200

    se  se mal vec UIja

--- App/TakeControl/TakeControl/Views/EditView/EditGroupView.swift
+++ App/TakeControl/TakeControl/Views/EditView/EditGroupView.swift
@@ -33,14 +33,15 @@ struct EditGroupView: View {
 
 
             }
-            .padding(.all)
-            .frame(height: 150)
+            .padding(.horizontal, 30)
+            .padding(.vertical)
+            .frame(height: 130)
             
             RoundedRectangle(cornerRadius: 10)
-                .padding(.horizontal, 40)
+                .padding(.horizontal, 30)
                 .foregroundColor(Color.white)
                 .shadow(color: Color.black.opacity(0.2), radius: 5, x: 2, y: 5)
-                .frame(height: 80)
+                .frame(height: 30)
         }
     }
 }
