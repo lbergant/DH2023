646eff9256cd050cb0a5028d3f5038aa69a6b71b
Luka Bergant <luka@bergant.com>
2023-05-13 21:50:16 +0200

    Implemented API calls for Calories

--- App/TakeControl/TakeControl/Health.swift
+++ App/TakeControl/TakeControl/Health.swift
@@ -23,7 +23,7 @@ struct Health: View {
                 ha.initHealthAPI()
             }
             Button("API request"){
-                ha.queryActivitySummary(startDate: Date.now, endDate: Date.now);
+                ha.queryActivitySummary();
             }
             
         }
@@ -57,7 +57,8 @@ struct HealthAPI{
     
     func requestHealthKitAuthorization() {
         // Define the types of data you want to read
-        let typesToRead: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .height)!]
+        let typesToRead: Set<HKObjectType> = [
+            HKObjectType.activitySummaryType()]
         
         // Request authorization from the user
         healthStore.requestAuthorization(toShare: [], read: typesToRead) { (success, error) in
@@ -65,19 +66,26 @@ struct HealthAPI{
         }
     }
     
-    func queryActivitySummary(startDate: Date, endDate: Date) {
+    func queryActivitySummary() {
         let calendar = Calendar.current
+        let endDate = Date.now
         //    let units: NSCalendar.Unit = [.day, .month, .year]
         
         
-        var startDateCom = calendar.dateComponents([.year, .month, .day], from: Date.distantPast)
+
+        
+        var endDateCom = calendar.dateComponents([.year, .month, .day], from: endDate)
+                endDateCom.calendar = calendar
+        
+        guard let startDate = calendar.date(byAdding: .day, value: -365, to: endDate) else {
+            fatalError("*** Unable to create the start date ***")
+        }
+        var startDateCom = calendar.dateComponents([.year, .month, .day], from:startDate)
         startDateCom.calendar = calendar
         
-        var endDateCom = calendar.dateComponents([.year, .month, .day], from: Date.now)
-        endDateCom.calendar = calendar
+        
         
         let predicate = HKQuery.predicate(forActivitySummariesBetweenStart: startDateCom, end: endDateCom)
-        //    let predicate = HKQuery.predicateForAct
         
         let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
             if let error = error {
@@ -98,16 +106,16 @@ struct HealthAPI{
                 //                    summary.dateComponents(for: calendar).date
                 
                 print("Active Energy Burned: \(summary.activeEnergyBurned.doubleValue(for: .kilocalorie())) kcal")
-                print("Exercise Time: \(summary.appleExerciseTime.doubleValue(for: .minute())) mins")
-                print("Stand Hours: \(summary.appleStandHours.doubleValue(for: .count())) hours")
-                print("Basal Energy Burned: \(summary.activeEnergyBurnedGoal.doubleValue(for: .count())) kcal")
+//                print("Exercise Time: \(summary.appleExerciseTime.doubleValue(for: .minute())) mins")
+//                print("Stand Hours: \(summary.appleStandHours.doubleValue(for: .count())) hours")
+                print("Energy Goal: \(summary.activeEnergyBurnedGoal.doubleValue(for: .count())) kcal")
                 //            print("Flights Climbed: \(summary.flightsClimbed.doubleValue(for: .count())) flights")
-                //            print("Step Count: \(summary.stepCount.doubleValue(for: .count())) steps")
+//                print("Step Count: \(summary.stepCount.doubleValue(for: .count())) steps")
                 //            print("Distance: \(summary.distance?.doubleValue(for: .meter()) ?? 0) meters")
             }
         }
         
-        HKHealthStore().execute(query)
+        healthStore.execute(query)
     }
     
 }
