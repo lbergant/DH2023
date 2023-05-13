8390a787c7320f5c5113859312e23a600c7346b3
Luka Bergant <luka@bergant.com>
2023-05-13 20:57:22 +0200

    Implemented HealthAPI struct with option to change outside @State

--- App/TakeControl/TakeControl/Health.swift
+++ App/TakeControl/TakeControl/Health.swift
@@ -11,17 +11,19 @@ import HealthKit
 let healthStore = HKHealthStore()
 
 struct Health: View {
-    @State var result : String = "";
+    @State var result = "";
     
     var body: some View {
+        let ha = HealthAPI.init(buttonText: $result)
+        
         VStack{
             Text("Health")
             Text(result)
             Button("Init health API") {
-                initHealthAPI()
+                ha.initHealthAPI()
             }
             Button("API request"){
-                queryActivitySummary(startDate: Date.now, endDate: Date.now);
+                ha.queryActivitySummary(startDate: Date.now, endDate: Date.now);
             }
             
         }
@@ -36,79 +38,76 @@ struct Health_Previews: PreviewProvider {
     }
 }
 
-func handleAPIAuthorization(status : Bool){
-    if status{
-        
-    }else{
-        
-    }
-}
-
-func initHealthAPI(){
-    if HKHealthStore.isHealthDataAvailable(){
-        requestHealthKitAuthorization()
-    }
-}
-
-func requestHealthKitAuthorization() {
-    // Define the types of data you want to read
-    let typesToRead: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .height)!]
+struct HealthAPI{
+    @Binding var buttonText: String
     
-    // Request authorization from the user
-    healthStore.requestAuthorization(toShare: [], read: typesToRead) { (success, error) in
-        if success {
-            // User authorized access to health data
-        } else {
-            // Authorization failed or was denied by the user
+    func handleAPIAuthorization(status : Bool){
+        if status{
+            buttonText = "OK"
+        }else{
+            buttonText = "NOK"
+        }
+    }
+    
+    func initHealthAPI(){
+        if HKHealthStore.isHealthDataAvailable(){
+            requestHealthKitAuthorization()
+        }
+    }
+    
+    func requestHealthKitAuthorization() {
+        // Define the types of data you want to read
+        let typesToRead: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .height)!]
+        
+        // Request authorization from the user
+        healthStore.requestAuthorization(toShare: [], read: typesToRead) { (success, error) in
+            handleAPIAuthorization(status: success)
+        }
+    }
+    
+    func queryActivitySummary(startDate: Date, endDate: Date) {
+        let calendar = Calendar.current
+        //    let units: NSCalendar.Unit = [.day, .month, .year]
+        
+        
+        var startDateCom = calendar.dateComponents([.year, .month, .day], from: Date.distantPast)
+        startDateCom.calendar = calendar
+        
+        var endDateCom = calendar.dateComponents([.year, .month, .day], from: Date.now)
+        endDateCom.calendar = calendar
+        
+        let predicate = HKQuery.predicate(forActivitySummariesBetweenStart: startDateCom, end: endDateCom)
+        //    let predicate = HKQuery.predicateForAct
+        
+        let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
             if let error = error {
-                print("Authorization failed. Error: \(error.localizedDescription)")
+                print("Error querying activity summaries: \(error.localizedDescription)")
+                return
             }
-        }
-    }
-}
-
-func queryActivitySummary(startDate: Date, endDate: Date) {
-    let calendar = Calendar.current
-//    let units: NSCalendar.Unit = [.day, .month, .year]
-    
-    
-    var startDateCom = calendar.dateComponents([.year, .month, .day], from: Date.distantPast)
-    startDateCom.calendar = calendar
-    
-    var endDateCom = calendar.dateComponents([.year, .month, .day], from: Date.now)
-    endDateCom.calendar = calendar
-    
-    let predicate = HKQuery.predicate(forActivitySummariesBetweenStart: startDateCom, end: endDateCom)
-//    let predicate = HKQuery.predicateForAct
-    
-    let query = HKActivitySummaryQuery(predicate: predicate) { (query, summaries, error) in
-        if let error = error {
-            print("Error querying activity summaries: \(error.localizedDescription)")
-            return
-        }
-        
-        guard let summaries = summaries else {
-            return
-        }
-        
-        for summary in summaries {
-            // Access the activity summary data
-            if let date = calendar.date(from: summary.dateComponents(for: calendar)){
-                print("Date: \(date.description)")
-            }
-                    
-//                    summary.dateComponents(for: calendar).date
             
-            print("Active Energy Burned: \(summary.activeEnergyBurned.doubleValue(for: .kilocalorie())) kcal")
-            print("Exercise Time: \(summary.appleExerciseTime.doubleValue(for: .minute())) mins")
-            print("Stand Hours: \(summary.appleStandHours.doubleValue(for: .count())) hours")
-            print("Basal Energy Burned: \(summary.activeEnergyBurnedGoal.doubleValue(for: .count())) kcal")
-//            print("Flights Climbed: \(summary.flightsClimbed.doubleValue(for: .count())) flights")
-//            print("Step Count: \(summary.stepCount.doubleValue(for: .count())) steps")
-//            print("Distance: \(summary.distance?.doubleValue(for: .meter()) ?? 0) meters")
+            guard let summaries = summaries else {
+                return
+            }
+            
+            for summary in summaries {
+                // Access the activity summary data
+                if let date = calendar.date(from: summary.dateComponents(for: calendar)){
+                    print("Date: \(date.description)")
+                }
+                
+                //                    summary.dateComponents(for: calendar).date
+                
+                print("Active Energy Burned: \(summary.activeEnergyBurned.doubleValue(for: .kilocalorie())) kcal")
+                print("Exercise Time: \(summary.appleExerciseTime.doubleValue(for: .minute())) mins")
+                print("Stand Hours: \(summary.appleStandHours.doubleValue(for: .count())) hours")
+                print("Basal Energy Burned: \(summary.activeEnergyBurnedGoal.doubleValue(for: .count())) kcal")
+                //            print("Flights Climbed: \(summary.flightsClimbed.doubleValue(for: .count())) flights")
+                //            print("Step Count: \(summary.stepCount.doubleValue(for: .count())) steps")
+                //            print("Distance: \(summary.distance?.doubleValue(for: .meter()) ?? 0) meters")
+            }
         }
+        
+        HKHealthStore().execute(query)
     }
     
-    HKHealthStore().execute(query)
 }
-
