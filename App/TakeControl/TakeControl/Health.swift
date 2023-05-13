fb4a0d4991e99b72e0264023b97eaf66e2bf4251
Luka Bergant <luka@bergant.com>
2023-05-13 15:10:51 +0200

    Added Tab view with Home, Health and Screen time pages

---
+++ App/TakeControl/TakeControl/Health.swift
@@ -0,0 +1,20 @@
+//
+//  Health.swift
+//  TakeControl
+//
+//  Created by Luka Bergant on 13/05/2023.
+//
+
+import SwiftUI
+
+struct Health: View {
+    var body: some View {
+        Text("Health")
+    }
+}
+
+struct Health_Previews: PreviewProvider {
+    static var previews: some View {
+        Health()
+    }
+}
---
+++ App/TakeControl/TakeControl/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  TakeControl
+//
+//  Created by Luka Bergant on 13/05/2023.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
